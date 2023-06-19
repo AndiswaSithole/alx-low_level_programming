df2a830c70a01de032f5dc09d599cb4c40eccc00
AndiswaSithole <jobekazie@gmail.cm>
2023-06-18 19:58:06 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - prints all strings  from 0 to 9
- * Return: Always 0
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int num;
-
-	for (num = 0; num < 10; num++)
-		printf("%d", num);
-	
-	printf('\n');
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
 	return (0);
 }
