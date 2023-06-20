03b0181472266a15a0dca53e701b7339584ea223
AndiswaSithole <jobekazie@gmail.cm>
2023-06-20 09:38:14 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <stdio.h> 
 
 /**
  * main - Entry point
@@ -10,7 +10,7 @@ int main(void)
 {
 	int i;
 
-	for (i=0; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		printf("%d", i);
 	}
