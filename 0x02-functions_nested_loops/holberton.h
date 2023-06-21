c20249996a9d8c4a0a44a65c429a79df7d0bc6ac
AndiswaSithole <jobekazie@gmail.cm>
2023-06-21 09:45:40 -0700

    ds

---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,17 @@
+#lfndet HOLBERTON_H
+#define HOLBERTON_H
+
+char _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int prints_last_digit(int nld);
+void jack_bauer(void);
+void times_table(void);
+int add(int one, int two);
+void print_to_98(int n);
+voide print_times_table(int n);
+
+#endif /* HOLBERTON_H */
